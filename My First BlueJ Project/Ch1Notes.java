17c2f00886efba9b08b6d321f0175c4d0bad1280
nwrooney <nwrooney@stu.naperville203.org>
2022-08-23 15:08:48 -0500

    First BlueJ Project
    Learned some useful Java tools

---
+++ My First BlueJ Project/Ch1Notes.java
@@ -0,0 +1,40 @@
+
+/**
+ * Noah Rooney
+ * 8/23/22
+ * Chapter 1 Notes
+ * Class notes on basic concepts
+ * Who helped me: Mr. Kischuk, Mr. Hayes, Dog
+ * 
+ 
+ 
+ 
+*/
+public class Ch1Notes
+{
+    
+    /**
+     * This is our code notes
+     */
+    public static void main(String[] args)
+    {
+        //basic data types
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        float number3 = 8;
+        long n2 =3;
+        short n3 = 7;
+        
+        //These are Classes and Objects
+        //Class     ID              Memory      Constructor
+        //Name
+        Integer     n1 =            new         Integer(47);
+        String      word =          new         String("Noah Rooney");
+        Double      number4 =       new         Double(6.7);
+        
+        System.out.print(word.length());
+        
+    }
+}
